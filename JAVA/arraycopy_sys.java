593a0d4e9724ae90e5140e02d8802c04fa16d8b1
alton15 <alton15@naver.com>
2018-08-21 23:24:51 +0900

    배열 복사

---
+++ JAVA/arraycopy_sys.java
@@ -0,0 +1,16 @@
+package ¿¬½À;
+
+public class arraycopy_sys {
+
+	public static void main(String[] args) {
+		String[] oldStrArr = {"java", "array", "copy"};
+		String[] newStrArr = new String[5];
+		
+		System.arraycopy(oldStrArr, 0, newStrArr, 0, oldStrArr.length);
+		
+		for(int i = 0; i < newStrArr.length; i++) {
+			System.out.print(newStrArr[i] + " ");
+		}
+	}
+
+}
