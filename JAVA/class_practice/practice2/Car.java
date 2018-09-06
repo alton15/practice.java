f3a538444b6d546162dc795c7877710b51853a0f
alton15 <alton15@naver.com>
2018-09-06 20:02:05 +0900

    CarOrder.java에 필요한 클래스

---
+++ JAVA/class_practice/practice2/Car.java
@@ -0,0 +1,15 @@
+package practice2;
+
+public class Car {
+	String Car_type;
+	int Fuel_efficiency;
+	int Price;
+	String Color;
+	
+	public Car(String tp, int f, int p, String c) {
+		Car_type = tp;
+		Fuel_efficiency = f;
+		Price = p;
+		Color = c;
+	}
+}
