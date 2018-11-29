ec4816532a859a37cadc5cce9acc4ad508250179
alton15 <alton15@naver.com>
2018-11-29 22:54:45 +0900

    11월 29일

---
+++ JAVA/new_start/11M29D/CarExN.java
@@ -0,0 +1,8 @@
+package prac_class;
+
+public class CarExN {
+	public static void main(String[] args) {
+		CarN myCarN = new CarN("검정", 3000);
+		//CarN myCarN = new CarN(); 과 같이 기본 생성자를 호출할 수 없다.
+	}
+}
